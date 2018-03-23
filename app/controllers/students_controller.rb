8ae46dde8ce2584144e8c9321b1dd7a67d49dcb8
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-03-23 15:17:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params)
+    @student = Student.new(params.require(:student))
     @student.save
     redirect_to student_path(@student)
   end
