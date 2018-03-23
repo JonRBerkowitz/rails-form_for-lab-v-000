cf0f1be61d083fc9312fb344283c9a1b04c9d217
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-03-23 14:29:10 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,18 @@
 class StudentsController < ApplicationController
   def show
+    @student = Student.find(params[:id])
   end
 
   def new
+    @student = Student.new
   end
 
   def edit
+  end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
